66df6d734e007039acb9fd6feb02c29175351ca2
Arnaud <arnaud.terret@gmail.com>
2021-10-20 06:40:25 +0200

    First commit

---
+++ build/precache-manifest.e177d15dc7a94f8c7a20c4ea06364720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba89a1703d7b279fec6c8a2ef2f51af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33d053f362095cdea952",
+    "url": "/static/css/main.411d37b7.chunk.css"
+  },
+  {
+    "revision": "79a8234d9216120990a1",
+    "url": "/static/js/2.74bfbd80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74bfbd80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d053f362095cdea952",
+    "url": "/static/js/main.942243a4.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);
